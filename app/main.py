9fe69ea3578f654ebd96ee5f82213f2a2c186bd2
Evgeniy Sergunin <sergunin.evgeniy@wb.ru>
2024-02-29 21:06:01 +0300

    форматинг

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 
 from config import TOKEN_TELEGRAM
+
+
 from database import db_user, User
 from integration_ai import get_ai_gen_text, get_ai_gen_image
 
@@ -11,25 +13,34 @@ from integration_ai import get_ai_gen_text, get_ai_gen_image
 bot = telebot.TeleBot(TOKEN_TELEGRAM, parse_mode=None)
 
 
-@bot.message_handler(commands=['start', 'help', 'asd'])
+@bot.message_handler(commands=["start", "help", "asd"])
 def send_welcome(message):
-    # user = User(message.from_user.username, message.from_user.id)
+    # user = User(message.from_user.userna
+    #
+    #
+    #
+    # me, message.from_user.id)
     # db_user.add_user(user)
 
     # Создаем клавиатуру
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    markup = types.ReplyKeyboardMarkup(
+        resize_keyboard=True, one_time_keyboard=True, row_width=2, selective=True
+    )
     # Добавляем кнопки
-    button1 = types.KeyboardButton('Котик')
-    button2 = types.KeyboardButton('Песик')
+    button1 = types.KeyboardButton("Котик")
+    button2 = types.KeyboardButton("Песик")
     markup.add(button1, button2)
 
     # Отправляем сообщение с прикрепленной клавиатурой
     bot.reply_to(message, "Howdy, how are you doing?", reply_markup=markup)
 
-@bot.message_handler(func=lambda message: message.text in ['Котик', 'Песик'])
+
+@bot.message_handler(func=lambda message: message.text in ["Котик", "Песик"])
 def cat_and_dog(message):
-    pront = get_ai_gen_text(f'Опиши картину с {message.text.lower()} очень веселую и забавную,'
-                            ' на заказ, четкие инструкции, пожалуйста.')
+    pront = get_ai_gen_text(
+        f"Опиши картину с {message.text.lower()} очень веселую и забавную,"
+        " на заказ, четкие инструкции, пожалуйста."
+    )
     url_image = get_ai_gen_image(pront)
     bot.send_photo(message.chat.id, url_image, caption=pront)
 
